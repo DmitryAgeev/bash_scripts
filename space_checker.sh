60a29a3e2aa2b3d5142c4efeb3b7ef329d6f91f8
Dmitry Ageev <dimasikk99@gmail.com>
2021-02-26 17:41:07 +0300

    Update space_checker.sh

--- space_checker.sh
+++ space_checker.sh
@@ -2,7 +2,7 @@
 # каждым пользователем дисковое пространство в его домашней директории.
 # текст скрипта по адресу: /home/krotbrod/sandbox/script.sh
 
-#!/bin/bash
+!/bin/bash
 
 # сначала скрипт узнаёт дату, затем размер всех домашних папок в кб 
 # некоторые файлы в поддиректориях недоступны для скрипта, потому что недостаточно 
@@ -14,8 +14,8 @@ day=$(date)
 size=$(du -s /home/* 2>&1)
 echo -e "$day\n$size\n\n\n" 
 
-$ crontab -e
-SHELL = /bin/sh
-MAILTO = krotkolobrod@gmail.com
-PATH = /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/home/krotbrod/sandbox
-28 15 * * * $HOME/sandbox/script.sh >> $HOME/sandbox/sizelog.txt
+# $ crontab -e
+# SHELL = /bin/sh
+# MAILTO = krotkolobrod@gmail.com
+# PATH = /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/home/krotbrod/sandbox
+# 28 15 * * * $HOME/sandbox/script.sh >> $HOME/sandbox/sizelog.txt
